b32f4b2fad5a0db26b7efdddd3e11266d09e36ff
mgkukielka <kokakol2>
2018-04-18 16:37:40 +0200

    + dodany FIlm i Sala

---
+++ src/sample/Sala.java
@@ -0,0 +1,35 @@
+package sample;
+
+public class Sala {
+    private String typ;
+    private String numer;
+    private Integer miejsca; // liczba miejsc
+
+    public Sala(String numer, String type, Integer miejsca) {
+        this.numer = numer;
+    }
+
+    public String getTyp() {
+        return typ;
+    }
+
+    public void setTyp(String typ) {
+        this.typ = typ;
+    }
+
+    public String getNumer() {
+        return numer;
+    }
+
+    public void setNumer(String numer) {
+        this.numer = numer;
+    }
+
+    public Integer getMiejsca() {
+        return miejsca;
+    }
+
+    public void setMiejsca(Integer miejsca) {
+        this.miejsca = miejsca;
+    }
+}
