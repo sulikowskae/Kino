06649ca91ed1f6cd4569860457c894294c2efd1c
mgkukielka <kokakol2>
2018-05-10 16:58:51 +0200

    + Seans

---
+++ src/sample/Seans.java
@@ -0,0 +1,16 @@
+package sample;
+
+import java.util.Date;
+
+public class Seans {
+    private Sala sala;
+    private Film film;
+    private Date date;
+
+    public Seans(Film film, Sala sala, Date date) {
+        this.film=film;
+        this.sala=sala;
+        this.date=date;
+    }
+    
+}
